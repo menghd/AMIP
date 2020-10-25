46e36d432e21ab07031b07a7491ab8b41d4375dd
menghd <souray@qq.com>
2020-10-25 14:02:23 +0800

    change

--- 3_ImageRepresentation/ConvertColorImage_3.m
+++ 3_ImageRepresentation/ConvertColorImage_3.m
@@ -1,9 +1,10 @@
+clear % 清空习惯
 colorimg = imread('PET_image.jpg');
 size(colorimg) % 读取尺寸用于创建灰度图
 grayimg = zeros(652, 1417);
 for i = 1:652
   for j = 1:1417
-    grayimg = 0.3*colorimg(i, j, 1) + 0.59*colorimg(i, j, 2) + 0.11*colorimg(i, j, 3); % 将三张RGB图转换为灰度图
+    grayimg(i, j) = 0.3*colorimg(i, j, 1) + 0.59*colorimg(i, j, 2) + 0.11*colorimg(i, j, 3); % 将RGB图对应值依次转换为灰度值
   end
 end
 
@@ -14,6 +15,6 @@ MaxValue = max(max(grayimg)); % 此处的grayimg已发生改变
 grayimg = floor(grayimg/MaxValue*64);
 
 colormap(gray)
-image(graying)
+image(grayimg)
 
 
