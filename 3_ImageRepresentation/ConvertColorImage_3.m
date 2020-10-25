8432ace896f1d5b7d0da6d5f6f04414b1894d882
menghd <souray@qq.com>
2020-10-25 14:09:37 +0800

    good habits

--- 3_ImageRepresentation/ConvertColorImage_3.m
+++ 3_ImageRepresentation/ConvertColorImage_3.m
@@ -4,9 +4,10 @@ size(colorimg) % 读取尺寸用于创建灰度图
 grayimg = zeros(652, 1417);
 for i = 1:652
   for j = 1:1417
-    grayimg(i, j) = 0.3*colorimg(i, j, 1) + 0.59*colorimg(i, j, 2) + 0.11*colorimg(i, j, 3); % 将RGB图对应值依次转换为灰度值
+    grayimg(i, j) = 0.3*colorimg(i, j, 1) + 0.59*colorimg(i, j, 2) + 0.11*colorimg(i, j, 3); % 将RGB对应值逐个转为灰度值
   end
-end
+end % 可利用':'简写矩阵循环
+% grayimg(:) = .3*colorimg(:,:,1) + .59*colorimg(:,:,2) +.11*colorimg(:,:,3);
 
 % 转换为64灰度值
 MinmumValue = min(min(grayimg));
