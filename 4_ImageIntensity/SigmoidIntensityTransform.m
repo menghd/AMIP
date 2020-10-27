bf07466337991246f50527435f52dd5178dd970e
menghd <souray@qq.com>
2020-10-27 21:01:08 +0800

    sigmoid

---
+++ 4_ImageIntensity/SigmoidIntensityTransform.m
@@ -0,0 +1,16 @@
+clear
+oimg = imread('ABD_CT.jpg');
+size(oimg) % 取出图像尺寸
+
+% 生成Sigmoid处理向量
+pomega = 127;
+psigma = 25;
+sigmoid = zeros(256, 1);
+for rho = 0:255
+  sigmoid(rho + 1, 1) = 128*1/(1 + exp(-((rho -pomega)/psigma))); % 注意rho与向量计数方式的差别
+end
+% plot(sigmoid)
+transimage = zeros(261, 435);
+transimage(:) =  sigmoid(oimg(:) + 1, 1);
+colormap(gray)
+img(transimage)
